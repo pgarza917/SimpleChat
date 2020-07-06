b3b1524f6ce9b53ebf4831005afa82a0671f8d36
Pablo Garza <pgarza917@fb.com>
2020-07-06 13:39:51 -0400

    UI improvement, anonymous login, and message save added

--- app/src/main/java/com/example/simplechat/ChatActivity.java
+++ app/src/main/java/com/example/simplechat/ChatActivity.java
@@ -3,12 +3,86 @@ package com.example.simplechat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class ChatActivity extends AppCompatActivity {
+
+    public static final String TAG = ChatActivity.class.getSimpleName();
+    static final String USER_ID_KEY = "userId";
+    static final String BODY_KEY = "body";
+
+    Button btSend;
+    EditText etMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        // User login
+        // Start with existing user if it exists
+        if (ParseUser.getCurrentUser() == null) {
+            startWithCurrentUser();
+        } else {    // If not logged in, login as a new anonymous user
+            login();
+        }
+    }
+
+    // Create an anonymous user using the ParseAnonymousUtils and set UserId
+    void login() {
+        ParseAnonymousUtils.logIn(new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e != null) {
+                    Log.e(TAG, "Anonymous login failed: ", e);
+                } else {
+                    startWithCurrentUser();
+                }
+            }
+        });
+    }
+
+    // Get the userId from the cached currentUser object
+    void startWithCurrentUser() {
+        setupMessagePosting();
+    }
+
+    // Setup the button event handler that will post the entered messages to Parse
+    void setupMessagePosting() {
+        // Get references to button and edit text in activity chat
+        btSend = findViewById(R.id.btSend);
+        etMessage = findViewById(R.id.etMessage);
+
+        // When the button is clicked, create message object on Parse
+        btSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = etMessage.getText().toString();
+                ParseObject message = new ParseObject("message");
+                message.add(USER_ID_KEY, ParseUser.getCurrentUser().getObjectId());
+                message.add(BODY_KEY, data);
+                message.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e == null) {
+                            Toast.makeText(ChatActivity.this, "Successfully created message on Parse", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.e(TAG, "Failed to save message", e);
+                        }
+                    }
+                });
+            }
+        });
     }
 }
